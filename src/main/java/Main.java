1773e8cd88a76ebd21eb3f64207bb6dc26134962
Aapo <aapo.sirius.salminen9@gmail.com>
2024-09-23 10:28:01 +0300

    good 3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,12 +9,14 @@ public class Main {
         Driver driver2 = new Driver("Jorma", 1);
         Car car1 = new Car("Toyota" );
         Car car2 = new Car("BMW");
+        driver1.getCars().add(car1);
+        driver1.getCars().add(car2);
         DriverDao driverDao = new DriverDao();
         CarDao carDao = new CarDao();
-        driverDao.persist(driver1);
-        driverDao.persist(driver2);
         carDao.persist(car1);
         carDao.persist(car2);
+        driverDao.persist(driver1);
+        driverDao.persist(driver2);
 
        // System.out.println(driver1.getCars());
     }
