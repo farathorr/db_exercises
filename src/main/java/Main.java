5f6bb8bf005e3a6463f328c7088cca578f83e76b
Aapo <aapo.sirius.salminen9@gmail.com>
2024-09-23 10:32:16 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,17 +7,22 @@ public class Main {
     public static void main(String[] args) {
         Driver driver1 = new Driver("Aapo", 99);
         Driver driver2 = new Driver("Jorma", 1);
+
         Car car1 = new Car("Toyota" );
         Car car2 = new Car("BMW");
+
         driver1.getCars().add(car1);
         driver1.getCars().add(car2);
+
         DriverDao driverDao = new DriverDao();
         CarDao carDao = new CarDao();
+
         carDao.persist(car1);
         carDao.persist(car2);
+
         driverDao.persist(driver1);
         driverDao.persist(driver2);
 
-       // System.out.println(driver1.getCars());
+        System.out.println(driverDao.find(1).getCars());
     }
 }
